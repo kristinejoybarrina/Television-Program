9aee51691fb1267b737920c64976c36976a11774
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-05-13 16:45:55 +0800

    Pseudocode

---
+++ Test Television.py
@@ -0,0 +1,10 @@
+# Kristine Joy Barrina #BSCPE 1-5 # May 13, 2023
+# Create a Python Code for creating the Class and a Test Driver program that will create two objects and produce output.
+
+# Import Class Television to Test Television program
+# Create a working class 
+# Define a function in Test Television program
+# Call all methods and print their function
+# Create two objects
+# Call instance method and print output
+# Display it in tkinter
