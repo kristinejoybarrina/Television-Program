235c7065f1166ff1ab24c1d1591963b5388f0c3e
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-05-13 17:33:09 +0800

    Import class to main program - tried if it can access class file

---
+++ Television.py
@@ -0,0 +1,11 @@
+# Create a class 
+
+class Television:
+    
+    def __init__(self) -> None:
+        self
+        pass
+    
+    def try_lang (self):
+        self.try_natin = "yay working na!"
+        return self.try_natin
--- Test Television.py
+++ Test Television.py
@@ -2,6 +2,12 @@
 # Create a Python Code for creating the Class and a Test Driver program that will create two objects and produce output.
 
 # Import Class Television to Test Television program
+from Television import Television
+
+television1 = Television()
+print(television1.try_lang())
+
+
 # Create a working class 
 # Define a function in Test Television program
 # Call all methods and print their function
