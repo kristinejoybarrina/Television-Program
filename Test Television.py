71bf7f161ca902c7569f7f621289541337ff4093
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-05-15 13:36:07 +0800

    Add string that shows what channel it is

--- Test Television.py
+++ Test Television.py
@@ -5,7 +5,8 @@
 from Television import Television
 
 # only trying to print just to check it's working
-television1 = Television(1, 2, 1)
+television1 = Television(30, 3, 1)
+television2 = Television(3, 2, 1)
 print(television1.turn_on(), television1.turn_off(), television1.get_channel(), television1.set_channel(), television1.get_volume(), television1.set_volume(), television1.channel_up(), television1.channel_down(), television1.volume_up(), television1.volume_down())
 
 # Define a function in Test Television program
